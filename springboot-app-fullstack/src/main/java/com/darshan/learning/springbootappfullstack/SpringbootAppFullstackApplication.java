74756027b85c5d1ebf14cac8c9320404abd5dc2e
darshan2494 <sanghavi.da@husky.neu.edu>
2018-08-07 14:34:54 -0400

    Spring boot initialization

---
+++ springboot-app-fullstack/src/main/java/com/darshan/learning/springbootappfullstack/SpringbootAppFullstackApplication.java
@@ -0,0 +1,12 @@
+package com.darshan.learning.springbootappfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppFullstackApplication.class, args);
+	}
+}
